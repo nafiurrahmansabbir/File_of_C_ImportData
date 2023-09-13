49480ba660446cd804d2b7f59baf00924b615613
Nafiur Rahman Sabbir <nafiurrahmansabbir@gmail.com>
2023-09-14 01:26:35 +0600

    second  commit

--- Demo2.c
+++ Demo2.c
@@ -1,1 +1,22 @@
-#include<stdio.
+#include<stdio.h>
+#include<string.h>
+int main(){
+    FILE *file;
+    char name[20]="Nafiur Rahman S dsfd";
+    int lenght=strlen(name);
+    int i;
+    
+    file=fopen("test.txt","a");
+    if(file==NULL){
+        printf("File Doesn't exit\n");
+    }else{
+        printf("File is created\n");
+        for(i=0;i<lenght;i++){
+            fputc(name[i],file);
+        }
+        fputs("\n",file); //  \n
+        fclose(file);
+    }
+
+    return 0;
+} 
